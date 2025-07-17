f9bf5d39cd278d2930e5437880a567008b3ccbe6
dynastybuildersacademy <131524002+dynastybuildersacademy@users.noreply.github.com>
2025-07-17 08:39:34 -0700

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,6 @@
 import 'package:flutter/material.dart';
-import 'screens/dashboard_screen.dart';
-import 'screens/prospects_screen.dart';
-import 'screens/appointments_screen.dart';
-import 'screens/settings_screen.dart';
 
-void main() {
-  runApp(CallTrackerApp());
-}
+void main() => runApp(CallTrackerApp());
 
 class CallTrackerApp extends StatelessWidget {
   @override
@@ -14,13 +8,10 @@ class CallTrackerApp extends StatelessWidget {
     return MaterialApp(
       title: 'Call Tracker+',
       theme: ThemeData.dark(),
-      initialRoute: '/',
-      routes: {
-        '/': (context) => DashboardScreen(),
-        '/prospects': (context) => ProspectsScreen(),
-        '/appointments': (context) => AppointmentsScreen(),
-        '/settings': (context) => SettingsScreen(),
-      },
+      home: Scaffold(
+        appBar: AppBar(title: Text('Call Tracker+')),
+        body: Center(child: Text('Welcome to Call Tracker+')),
+      ),
     );
   }
 }
